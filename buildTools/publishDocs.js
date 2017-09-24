a0967f2a295715247eade8a20481dd262a67d740
Matt Miller <matt.miller@brightbytes.net>
2017-09-24 09:04:43 -0600

    Always use latest version in docs redirect

--- buildTools/publishDocs.js
+++ buildTools/publishDocs.js
@@ -3,23 +3,29 @@ const ghPages = require('gh-pages')
 const parseArgs = require('minimist')
 const promisify = require('es6-promisify')
 const rimraf = require('rimraf')
+const sortVersions = require('semver-sort').asc
 const { spawn } = require('child_process')
 
 const INVALID_TAG = new Error('Tag is not a version tag')
 
 const publish = promisify(ghPages.publish)
 const mkdir = promisify(fs.mkdir)
+const readFile = promisify(fs.readFile)
 const rename = promisify(fs.rename)
 const rmdir = promisify(rimraf)
 const writeFile = promisify(fs.writeFile)
 const exec = (() => {
-  return function exec(command, args) {
+  return function exec(command, args, capture = false) {
     return new Promise((resolve, reject) => {
-      const childProcess = spawn(command, args, { stdio: 'inherit' })
+      let output = ''
+      const childProcess = spawn(command, args, capture ? undefined : { stdio: 'inherit' })
+      if (capture) {
+        childProcess.stdout.on('data', data => (output += data))
+      }
       childProcess.on('error', err => reject(err))
       childProcess.on('exit', code => {
         if (code === 0) {
-          resolve()
+          resolve(capture ? output : undefined)
         } else {
           reject(new Error(`Command ${command} ${args.join(' ')} returned a non-zero status code.`))
         }
@@ -43,6 +49,12 @@ async function run() {
   }
   const [, version] = match
 
+  await rmdir('tmp')
+
+  const pkg = JSON.parse(await readFile('package.json'))
+  const versions = sortVersions(JSON.parse(await exec('npm', ['view', pkg.name, 'versions', '--json'], true)))
+  const latestVersion = versions[versions.length - 1]
+
   await exec('npm', ['run', 'generate-docs'])
   await mkdir('tmp')
   await rename('docs', `tmp/${version}`)
@@ -54,7 +66,7 @@ async function run() {
   <head>
     <meta charset="utf-8">
     <script>
-      var version = ${JSON.stringify(version)}
+      var version = ${JSON.stringify(latestVersion)}
       var port = location.port ? ':' + location.port : ''
       var pathname = location.pathname.split('index.html')[0]
       location.href = location.protocol + '//' + location.hostname + port + pathname + version
